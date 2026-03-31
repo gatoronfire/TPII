cf2c1fafd0465556bea0ed223e95291c90dc5e9f
gatoronfire <69321343+gatoronfire@users.noreply.github.com>
2026-03-30 23:40:08 -0300

    comentarios
    Se añaden comentarios para entender cada parte y funcion del codigo

--- Main.java
+++ Main.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //hay un proximo int?
         if (!sc.hasNextInt()) {
             sc.close();
             return;
         }
+        //leer el primer int que me dice la cantidad de operaciones
         int cantidad = sc.nextInt();
+        //limpiar el buffer del scanner
         sc.nextLine();
+        //creo la pila con la cantidad de operaciones en caso que todas sean push
         Pila mipila = new Pila(cantidad);
+        //loop por la cantidad de operaciones
         for(int i =0; i < cantidad; i++){
+            //leo la linea que tiene formato "operacion-valor"
             String N = sc.nextLine();
+            //use if porque no me acordaba como usar switch 
             if(N.contains("POP")){
                 mipila.POP();
             }
             if(N.contains("PUSH")){
+                //extraigo el valor del push, lo convierto a int y lo paso a la funcion push
                 String valorStr = N.substring(5).trim();
                 mipila.PUSH(Integer.parseInt(valorStr));
             }
@@ -37,14 +45,21 @@ public class Main {
     }
 }
 
-
+//clase pila (quien lo iba a decir?)
+//la clase tiene las funciones que se usan en las pilas
 class Pila {
+    //arreglo de los elementos de la pila (datos principales)
         private int[] elementos;
+        //tope de la pila
         private int top;
+        //tamaño maximo de la pila
         private int cantidad;
+        //arreglo para guardar el maximo en cada nivel de la pila, asi no tengo que recorrer la pila cada vez que quiero saber el maximo
         private int[] max;
+        //era para ver los numeros negativos de algun ejercicio, no se uso
         private int[] neg;
         
+        //establecer el tamaño de la pila y crear los arreglos necesarios
         public Pila(int cantidad){
             this.cantidad = cantidad;
             this.elementos = new int[cantidad];
@@ -52,7 +67,7 @@ class Pila {
             this.max = new int [cantidad];
             this.neg = new int [cantidad];
         }
-
+        //funcion push. suma 1 al tope y agrega x en la posicion tope. tambien compara x con el maximo y actualiza el arreglo max
         public void PUSH(int x){
             top ++;
             elementos[top] = x;
@@ -67,10 +82,11 @@ class Pila {
             }
             
         }
-
+        //funcion top para imprimir el elemento en el tope de la pila
         public void TOP(){
             System.out.println(elementos[top]);
         }
+        //chequea si la lista esta vacia
         public void EMPTY(){
             if(top > -1){
                 System.out.println(0);
@@ -78,6 +94,7 @@ class Pila {
                 System.out.println(1);
             }
         }
+        //devuelve el tamaño de la pila (tope+1)
         public void SIZE(){
             if(top == -1){
                 System.out.println(0);
@@ -86,12 +103,15 @@ System.out.println(top + 1);
             }
             
         }
+        //imprime el utlimo elemento del arreglo max que guarda el elemento mas grande de la pila normal
         public void MAX(){
             System.out.println(max[top]);
         }
+        //resta 1 al tope entonces no podes acceder al elemento anterior
         public void POP(){
             top--;
         }
+        //imprime toda la pila. se uso para debuggear
         public void ALL(){
             for(int i =0; i<=top; i++){
                 System.out.println(elementos[i]);
