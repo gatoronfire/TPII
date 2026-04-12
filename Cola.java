ecdce2981011cf38525b8423c1e97cc15c638a26
diego <diego.casas11104@gmail.com>
2026-04-12 19:34:36 -0300

    creacion de constructor

--- Cola.java
+++ Cola.java
@@ -5,8 +5,13 @@ public class Cola {
     }
 }
 
-class cola {
-        public Cola(){
+class Cola {
+    private int[] cola;
+    private int tope;
+
+        public Cola(int N) {
+            this.cola = new int[N];
+            this.tope = -1;
         }
         public void PUSH(){
         }
