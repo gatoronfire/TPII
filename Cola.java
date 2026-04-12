e18c00182d9236dae90615cf22cc37eb2b1dee5b
diego <diego.casas11104@gmail.com>
2026-04-12 20:07:14 -0300

    agregado de int tamaño

--- Cola.java
+++ Cola.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Cola {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        cola c = new cola(N); 
     }
 }
 
@@ -9,11 +13,13 @@ class cola {
     private String[] cola;
     private int cabeza;
     private int fin;
+    private int size;
 
         public cola(int N) {
             this.cola = new String[N];
             this.cabeza = 0;
             this.fin = 0;
+            this.size = 0;
         }
         public void PUSH(){
         }
