adf95fe29a024e9872e5e6d5b54129ec148ef5dc
faustina <fenecoiz@alumnos.unsada.edu.ar>
2026-04-14 11:41:32 -0300

    Resuelto el ejercicio C

--- Torneo.java
+++ Torneo.java
@@ -6,26 +6,33 @@ public class Torneo {
         int N = sc.nextInt();
         sc.nextLine();
         //guardar los equipos en el arreglo al principio
-        Cola equipos = new Cola(N);
-
-
+        Zola equipos = new Zola(N);
 
         for (int i = 0; i < N; i++) {
-            String equipos = sc.nextLine();  
+            String equipo = sc.nextLine(); 
+            // lo guardo dentro del arreglo
+            equipos.PUSH(equipo);
         }
         int R = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < R; i++) {
-            String respuestas = sc.nextLine();            
+            String respuestas = sc.nextLine();
+            if(respuestas.equals("BIEN")){
+                equipos.PUSH(equipos.TOP());
+            } 
+            if(respuestas.equals("MAL")){
+                equipos.POP();
+            }         
         }
+        System.out.println(equipos.TOP());
     }
 }
-class Cola {
+class Zola {
     private String[] cola;
     private int inicio;
     private int fin;
 
-    public Cola(int N) {
+    public Zola(int N) {
         this.cola = new String[N];
         this.inicio = 0;
         this.fin = 0;
@@ -56,6 +63,7 @@ class Cola {
         cola[inicio] = null;
         if ((inicio + 1) == cola.length) {
             inicio = 0;
+            return;
         } else {
             inicio++;
         }
