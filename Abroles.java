903e8616af7ab8831a688fdc5f642ffb4aa558fd
faustina <fenecoiz@alumnos.unsada.edu.ar>
2026-05-22 11:36:17 -0300

    Arreglos en la funcion agregar hijo PROBAR!!!!!!!

--- Abroles.java
+++ Abroles.java
@@ -43,23 +43,35 @@ public class Abroles {
             }
         }
     }
+    //FUNCION AGREGAR HIJO(los menores a la izquierda, los mayores a la derecha)
+    //CONCEPTUALMENTE DEBO : 
+    // buscar donde debe ir el hijo dependiendo del valor
+    // mover los mayores una posicion a la derecha
+    // insertar el nuevo hijo 
 
     public static void agregarHijo(Nodo padre, Nodo hijo){
-        if(padre.hijos[0] == null){
-            padre.hijos[0] = hijo;
-        }else{
-            for(int i=1; i < padre.hijos.length; i++){
-                //busca una posicion vacia
-                if(padre.hijos[i].dato < hijo.dato){
-                    //agrega el hijo
-                    for(int j = i; j > 0; j--){
-                        
-                    }
-                    //le asigna el padre al hijo
-                }
-            }
-        }
-         hijo.padre = padre;   
+        //creo una variable donde va a quedar el nuevo hijo 
+        int posicion = 0;
+        //busco la posicion donde debe ir el hijo 
+            //mientras no me salga del arreglo
+            //mientras exista un hijo ahí
+            //mientras el dato del actual sea menor al q quiero insertar 
+        while(posicion < padre.hijos.length &&
+             padre.hijos[posicion] != null && 
+             padre.hijos[posicion].dato < hijo.dato){
+
+            posicion++;
+        } 
+        //mover los mayores a la derecha
+        //copia cada elemento , empezando por el final, lo mueve a la derecha 
+        for(int i = padre.hijos.length - 1; i > posicion; i--){
+            //guarda en el actual lo que tenia el anterior
+            padre.hijos[i] = padre.hijos[i-1];
+        }  
+        //insertar el nuevo hijo
+        padre.hijos[posicion] = hijo;
+        //le asigna el padre al hijo
+        hijo.padre = padre;
     }
     //Preorden: visitar el nodo actual, luego recorrer los hijos de izquierda a derecha
     public void preorden(Nodo nodo){
@@ -93,6 +105,16 @@ public class Abroles {
         // visitar el nodo actual
         System.out.println(nodo.dato);
     }
+    //Por nivel: logica FIFO, por ende debemos usar una cola
+    //A definir : crear una cola propia o usar QUEUE , una clase de java que viene bien para esto
+    public void pornivel(Nodo nodo){
+    }
+
+    //In orden: izquierda, nodo actual,derecha. 
+    //como no es un arbol binario podriamos primero recorrer:
+    //los hijos de la izquierda, luego el nodo actual y luego los hijos de la derecha
+    public void inorden(Nodo nodo){
+    }
     
 
 }
@@ -102,9 +124,33 @@ class Nodo {
     int dato;
     Nodo padre;
 
-    public Nodo(int Nodos, int dato, Nodo Padre) {
+    public Nodo(int Nodos, int dato, Nodo padre) {
         this.hijos = new Nodo[Nodos];
         this.dato = dato;
         this.padre = padre;
     }
 }
+
+
+/* funcion que hizo jordi, por las dudas 
+   public static void agregarHijo(Nodo padre, Nodo hijo){
+        //si no tiene nada, lo agrega, simple
+        if(padre.hijos[0] == null){
+            padre.hijos[0] = hijo;
+        }else{
+            for(int i=1; i < padre.hijos.length; i++){
+                //busca una posicion vacia
+                if(padre.hijos[i].dato < hijo.dato){
+                    //agrega el hijo
+                    for(int j = i; j > 0; j--){
+                        
+                    }
+                    //le asigna el padre al hijo
+                }
+            }
+        }
+         hijo.padre = padre;   
+    }
+
+
+*/
