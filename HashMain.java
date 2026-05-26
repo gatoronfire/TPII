3ef0437596d098685a613935fd797dadd47658a9
faustina <fenecoiz@alumnos.unsada.edu.ar>
2026-05-26 11:26:15 -0300

    creacion hashmain

---
+++ HashMain.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class HashMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Acá vamos a llamar a los otros hashes
+    }
+}
