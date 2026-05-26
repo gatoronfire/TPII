5218308eb837d062c0c4fdf8a4b64755fdd7c8e5
faustina <fenecoiz@alumnos.unsada.edu.ar>
2026-05-26 11:10:01 -0300

    creacion scanner

--- HashCuadratico.java
+++ HashCuadratico.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
 public class HashCuadratico {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
--- HashLineal.java
+++ HashLineal.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
 public class HashLineal {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
