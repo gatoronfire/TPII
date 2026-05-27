dd950bf622deb7a2c07a19663a5d6b5ca9b36dc8
diego <diego.casas11104@gmail.com>
2026-05-27 10:42:04 -0300

    eliminacion de funcion main

--- hash/HashEncadenamiento.java
+++ hash/HashEncadenamiento.java
@@ -1,7 +1,5 @@
 package hash;
 import java.util.Scanner;
 public class HashEncadenamiento {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
+    
 }
--- hash/HashLineal.java
+++ hash/HashLineal.java
@@ -2,7 +2,5 @@ package hash;
 import java.util.Scanner;
 
 public class HashLineal {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
+    
 }
