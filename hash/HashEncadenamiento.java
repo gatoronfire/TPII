77fde0d252e7ce0ffd12ec1cc4c40f71b3cfa9ef
faustina <fenecoiz@alumnos.unsada.edu.ar>
2026-06-02 10:47:16 -0300

    cambios en funcion encadenamiento

--- hash/HashEncadenamiento.java
+++ hash/HashEncadenamiento.java
@@ -35,19 +35,19 @@ public class HashEncadenamiento {
         System.out.println(total);
         sc.close();
     }
-    // funcion de hashing del powerpoint
-    public static int hashing(String palabra, int N) {
+  // funcion de hashing del powerpoint
+    public static int hashing(String palabra , int N) {
         int p = 151;
         long result = 0;
-        long potencia = 1;
+        int potencia = 1;
 
         for (int i = 0; i < palabra.length(); i++) {
 
-            result = (result + palabra.charAt(i) * potencia) % N;
+            result = (result + palabra.charAt(i)) * i;
 
-            potencia = (potencia * p) % N;
+            potencia = (potencia * p);
         }
-
+        result = result % N;
         return (int) result;
     }
 
